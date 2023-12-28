fee0d9db187d6e5e0dd90e0d1e139c2a6fa3fd86
seank <seankenny6688@gmail.com>
2023-12-27 19:16:43 -0700

    Moved API starting code to api/ route to match vercel's requirements on that

---
+++ api/graphql.js
@@ -0,0 +1,32 @@
+import { ApolloServer } from "apollo-server-micro";
+import { MongoClient } from "mongodb";
+import { arboTypedefs } from "../public/dist/src/api/arbo-typedefs.js";
+import { generateArboResolvers } from "../public/dist/src/api/arbo-resolvers.js";
+
+const mongoUrl = process.env.MONGODB_URI;
+
+if (!mongoUrl) {
+    console.log("Unable to find value for MONGODB_URI. Please make sure you have specified one in your .env file.");
+    console.log("Exiting early.");
+    process.exit(1);
+}
+
+const mongoClient = new MongoClient(mongoUrl);
+await mongoClient.connect();
+
+const server = new ApolloServer({
+    typeDefs: arboTypedefs,
+    resolvers: generateArboResolvers({ mongoClient }).arboResolvers,
+    introspection: true,
+});
+
+await server.start();
+export default server.createHandler({
+    path: "/api/graphql",
+});
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
