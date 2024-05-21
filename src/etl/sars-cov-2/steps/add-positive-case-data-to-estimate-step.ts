3dc4cb6120377ebf5a37068cca09e8c9b35dc901
seank <seankenny6688@gmail.com>
2024-05-21 12:25:16 -0600

    Improved our method of connecting vaccination and positive case data to estimates

--- src/etl/sars-cov-2/steps/add-positive-case-data-to-estimate-step.ts
+++ src/etl/sars-cov-2/steps/add-positive-case-data-to-estimate-step.ts
@@ -1,5 +1,115 @@
 import { MongoClient } from "mongodb";
-import { EstimateFieldsAfterAddingVaccinationDataStep, StructuredPositiveCaseDataAfterAddingVaccinationDataStep, StructuredVaccinationDataAfterAddingVaccinationDataStep, StudyFieldsAfterAddingVaccinationDataStep } from "./add-vaccination-data-to-estimate-step";
+import { add, sub } from 'date-fns';
+import {
+  EstimateFieldsAfterAddingVaccinationDataStep,
+  StructuredPositiveCaseDataAfterAddingVaccinationDataStep,
+  StructuredVaccinationDataAfterAddingVaccinationDataStep,
+  StudyFieldsAfterAddingVaccinationDataStep
+} from "./add-vaccination-data-to-estimate-step";
+import { StructuredPositiveCaseData } from "../types";
+
+interface GetAssociatedPositiveCaseDataForEstimateInput {
+  estimate: {
+    twoLetterCountryCode: string;
+    samplingMidDate: Date | undefined;
+  };
+  positiveCaseData: StructuredPositiveCaseData;
+  acceptableDayDifferenceRange: number;
+}
+
+interface GetAssociatedPositiveCaseDataForEstimateOutput {
+  countryPositiveCasesPerMillionPeople: number | undefined;
+}
+
+const getAssociatedPositiveCaseDataForEstimate = (input: GetAssociatedPositiveCaseDataForEstimateInput): GetAssociatedPositiveCaseDataForEstimateOutput => {
+  const { samplingMidDate } = input.estimate;
+
+  if(!samplingMidDate) {
+    return { countryPositiveCasesPerMillionPeople: undefined };
+  }
+
+  const acceptableDayDifferences = [...Array(input.acceptableDayDifferenceRange).keys()];
+
+  const dayDifferencesWithData = acceptableDayDifferences.flatMap((dayDifference) => {
+    const pastDateToEvaluate = sub(samplingMidDate, {days: dayDifference});
+    const futureDateToEvaluate = add(samplingMidDate, {days: dayDifference});
+
+    const dataForPastDateToEvaluate = input.positiveCaseData
+      .find((element) => element.twoLetterCountryCode === input.estimate.twoLetterCountryCode)?.data
+      .find((element) => element.year === pastDateToEvaluate.getUTCFullYear().toString())?.data
+      .find((element) => element.month === (pastDateToEvaluate.getUTCMonth() + 1).toString())?.data
+      .find((element) => element.day === (pastDateToEvaluate.getUTCDate()).toString());
+
+    const dataForFutureDateToEvaluate = input.positiveCaseData
+      .find((element) => element.twoLetterCountryCode === input.estimate.twoLetterCountryCode)?.data
+      .find((element) => element.year === futureDateToEvaluate.getUTCFullYear().toString())?.data
+      .find((element) => element.month === (futureDateToEvaluate.getUTCMonth() + 1).toString())?.data
+      .find((element) => element.day === (futureDateToEvaluate.getUTCDate()).toString());
+
+    return [(
+      (
+        dataForPastDateToEvaluate !== undefined &&
+        dataForPastDateToEvaluate.countryPositiveCasesPerMillionPeople !== undefined
+      ) ? {
+        dayDifference: -dayDifference,
+        data: {
+          countryPositiveCasesPerMillionPeople: dataForPastDateToEvaluate.countryPositiveCasesPerMillionPeople
+        }
+      } : undefined
+    ), (
+      (
+        dataForFutureDateToEvaluate !== undefined &&
+        dataForFutureDateToEvaluate.countryPositiveCasesPerMillionPeople !== undefined
+      ) ? {
+        dayDifference: dayDifference,
+        data: {
+          countryPositiveCasesPerMillionPeople: dataForFutureDateToEvaluate.countryPositiveCasesPerMillionPeople
+        }
+      } : undefined
+    )];
+  }).filter(<T extends unknown>(element: T | undefined): element is T => element !== undefined)
+
+  const dataForDay = dayDifferencesWithData.find((element) => element.dayDifference === 0)
+
+  if(!!dataForDay) {
+    return {
+      countryPositiveCasesPerMillionPeople: dataForDay.data.countryPositiveCasesPerMillionPeople
+    }
+  }
+
+  const dayInThePastWithTheLeastDifference = dayDifferencesWithData
+    .filter((element) => element.dayDifference < 0)
+    .sort((a, b) => Math.abs(a.dayDifference) - Math.abs(b.dayDifference))
+    .at(0);
+
+  const dayInTheFutureWithTheLeastDifference = dayDifferencesWithData
+    .filter((element) => element.dayDifference > 0)
+    .sort((a, b) => Math.abs(a.dayDifference) - Math.abs(b.dayDifference))
+    .at(0);
+  
+  if(dayInThePastWithTheLeastDifference && dayInTheFutureWithTheLeastDifference) {
+    // Turn this into a line with a slope of y=mx+b where x is the date.
+    // We approximate the line to be linear on such a small time scale (~14 days).
+
+    // m = dy/dx
+    const m = (dayInTheFutureWithTheLeastDifference.data.countryPositiveCasesPerMillionPeople - dayInThePastWithTheLeastDifference.data.countryPositiveCasesPerMillionPeople) / (dayInTheFutureWithTheLeastDifference.dayDifference - dayInThePastWithTheLeastDifference.dayDifference);
+
+    // b = y-mx
+    const b = (dayInTheFutureWithTheLeastDifference.data.countryPositiveCasesPerMillionPeople) - (m * dayInTheFutureWithTheLeastDifference.dayDifference)
+
+    // y = mx+b, x=0 in this case because the day difference is zero
+    const countryPositiveCasesPerMillionPeople = (m * 0) + b;
+
+    return {
+      countryPositiveCasesPerMillionPeople
+    }
+  }
+
+  // Getting here means we simply didn't have enough data to tell how many positive cases had occurred at this time.
+  return {
+    countryPositiveCasesPerMillionPeople: undefined
+  }
+}
 
 export type EstimateFieldsAfterAddingPositiveCaseDataStep =
   EstimateFieldsAfterAddingVaccinationDataStep & { 
@@ -31,18 +141,23 @@ interface AddPositiveCaseDataToEstimateStepOutput {
 export const addPositiveCaseDataToEstimateStep = (
   input: AddPositiveCaseDataToEstimateStepInput
 ): AddPositiveCaseDataToEstimateStepOutput => {
+  const acceptableDayDifferenceRange = 7;
+
   console.log(
-    `Running step: addPositiveCaseDataToEstimateStep. Remaining estimates: ${input.allEstimates.length}`
+    `Running step: addPositiveCaseDataToEstimateStep. Remaining estimates: ${input.allEstimates.length}. Acceptable day difference range: ${acceptableDayDifferenceRange}`
   );
 
   return {
     allEstimates: input.allEstimates.map((estimate) => ({
       ...estimate,
-      countryPositiveCasesPerMillionPeople: input.positiveCaseData
-        .find((element) => element.twoLetterCountryCode === estimate.countryAlphaTwoCode)?.data
-        .find((element) => estimate.samplingMidDate && element.year === estimate.samplingMidDate.getUTCFullYear().toString())?.data
-        .find((element) => estimate.samplingMidDate && element.month === (estimate.samplingMidDate.getUTCMonth() + 1).toString())?.data
-        .find((element) => estimate.samplingMidDate && element.day === (estimate.samplingMidDate.getUTCDate()).toString())?.countryPositiveCasesPerMillionPeople
+      countryPositiveCasesPerMillionPeople: getAssociatedPositiveCaseDataForEstimate({
+        estimate: {
+          twoLetterCountryCode: estimate.countryAlphaTwoCode,
+          samplingMidDate: estimate.samplingMidDate
+        },
+        positiveCaseData: input.positiveCaseData,
+        acceptableDayDifferenceRange
+      }).countryPositiveCasesPerMillionPeople
     })),
     allStudies: input.allStudies,
     vaccinationData: input.vaccinationData,
--- src/etl/sars-cov-2/steps/add-vaccination-data-to-estimate-step.ts
+++ src/etl/sars-cov-2/steps/add-vaccination-data-to-estimate-step.ts
@@ -1,10 +1,222 @@
 import { MongoClient } from "mongodb";
+import { add, sub } from 'date-fns';
 import {
   EstimateFieldsAfterJitteringPinLatLngStep,
   StructuredPositiveCaseDataAfterJitteringPinLatLngStep,
   StructuredVaccinationDataAfterJitteringPinLatLngStep,
   StudyFieldsAfterJitteringPinLatLngStep,
 } from "./jitter-pin-lat-lng-step";
+import { StructuredVaccinationData } from "../types";
+
+interface GetAssociatedVaccinationDataForEstimateInput {
+  estimate: {
+    threeLetterCountryCode: string;
+    samplingMidDate: Date | undefined;
+  };
+  vaccinationData: StructuredVaccinationData;
+  acceptableDayDifferenceRange: number;
+}
+
+interface GetAssociatedVaccinationDataForEstimateOutput {
+  countryPeopleVaccinatedPerHundred: number | undefined;
+}
+
+const getAssociatedVaccinationDataForEstimate = (input: GetAssociatedVaccinationDataForEstimateInput): GetAssociatedVaccinationDataForEstimateOutput => {
+  const { samplingMidDate } = input.estimate;
+
+  if(!samplingMidDate) {
+    return {
+      countryPeopleVaccinatedPerHundred: undefined,
+    };
+  }
+
+  const acceptableDayDifferences = [...Array(input.acceptableDayDifferenceRange).keys()];
+
+  const dayDifferencesWithData = acceptableDayDifferences.flatMap((dayDifference) => {
+    const pastDateToEvaluate = sub(samplingMidDate, {days: dayDifference});
+    const futureDateToEvaluate = add(samplingMidDate, {days: dayDifference});
+
+    const dataForPastDateToEvaluate = input.vaccinationData
+      .find((element) => element.threeLetterCountryCode === input.estimate.threeLetterCountryCode)?.data
+      .find((element) => element.year === pastDateToEvaluate.getUTCFullYear().toString())?.data
+      .find((element) => element.month === (pastDateToEvaluate.getUTCMonth() + 1).toString())?.data
+      .find((element) => element.day === (pastDateToEvaluate.getUTCDate()).toString());
+
+    const dataForFutureDateToEvaluate = input.vaccinationData
+      .find((element) => element.threeLetterCountryCode === input.estimate.threeLetterCountryCode)?.data
+      .find((element) => element.year === futureDateToEvaluate.getUTCFullYear().toString())?.data
+      .find((element) => element.month === (futureDateToEvaluate.getUTCMonth() + 1).toString())?.data
+      .find((element) => element.day === (futureDateToEvaluate.getUTCDate()).toString());
+
+    return [(
+      (
+        dataForPastDateToEvaluate !== undefined &&
+        dataForPastDateToEvaluate.countryPeopleVaccinatedPerHundred !== undefined
+      ) ? {
+        dayDifference: -dayDifference,
+        data: {
+          countryPeopleVaccinatedPerHundred: dataForPastDateToEvaluate.countryPeopleVaccinatedPerHundred,
+        }
+      } : undefined
+    ), (
+      (
+        dataForFutureDateToEvaluate !== undefined &&
+        dataForFutureDateToEvaluate.countryPeopleVaccinatedPerHundred !== undefined
+      ) ? {
+        dayDifference: dayDifference,
+        data: {
+          countryPeopleVaccinatedPerHundred: dataForFutureDateToEvaluate.countryPeopleVaccinatedPerHundred,
+        }
+      } : undefined
+    )];
+  }).filter(<T extends unknown>(element: T | undefined): element is T => element !== undefined)
+
+  const dataForDay = dayDifferencesWithData.find((element) => element.dayDifference === 0)
+
+  if(!!dataForDay) {
+    return {
+      countryPeopleVaccinatedPerHundred: dataForDay.data.countryPeopleVaccinatedPerHundred,
+    }
+  }
+
+  const dayInThePastWithTheLeastDifference = dayDifferencesWithData
+    .filter((element) => element.dayDifference < 0)
+    .sort((a, b) => Math.abs(a.dayDifference) - Math.abs(b.dayDifference))
+    .at(0);
+
+  const dayInTheFutureWithTheLeastDifference = dayDifferencesWithData
+    .filter((element) => element.dayDifference > 0)
+    .sort((a, b) => Math.abs(a.dayDifference) - Math.abs(b.dayDifference))
+    .at(0);
+  
+  if(dayInThePastWithTheLeastDifference && dayInTheFutureWithTheLeastDifference) {
+    // Turn this into a line with a slope of y=mx+b where x is the date.
+    // We approximate the line to be linear on such a small time scale (~14 days).
+
+    // m = dy/dx
+    const m = (dayInTheFutureWithTheLeastDifference.data.countryPeopleVaccinatedPerHundred - dayInThePastWithTheLeastDifference.data.countryPeopleVaccinatedPerHundred) / (dayInTheFutureWithTheLeastDifference.dayDifference - dayInThePastWithTheLeastDifference.dayDifference);
+
+    // b = y-mx
+    const b = (dayInTheFutureWithTheLeastDifference.data.countryPeopleVaccinatedPerHundred) - (m * dayInTheFutureWithTheLeastDifference.dayDifference)
+
+    // y = mx+b, x=0 in this case because the day difference is zero
+    const countryPeopleVaccinatedPerHundred = (m * 0) + b;
+
+    return {
+      countryPeopleVaccinatedPerHundred
+    }
+  }
+
+  // Getting here means we simply didn't have enough data to tell how many positive cases had occurred at this time.
+  return {
+    countryPeopleVaccinatedPerHundred: undefined
+  }
+}
+
+interface GetAssociatedFullVaccinationDataForEstimateInput {
+  estimate: {
+    threeLetterCountryCode: string;
+    samplingMidDate: Date | undefined;
+  };
+  vaccinationData: StructuredVaccinationData;
+  acceptableDayDifferenceRange: number;
+}
+
+interface GetAssociatedFullVaccinationDataForEstimateOutput {
+  countryPeopleFullyVaccinatedPerHundred: number | undefined;
+}
+
+const getAssociatedFullVaccinationDataForEstimate = (input: GetAssociatedFullVaccinationDataForEstimateInput): GetAssociatedFullVaccinationDataForEstimateOutput => {
+  const { samplingMidDate } = input.estimate;
+
+  if(!samplingMidDate) {
+    return {
+      countryPeopleFullyVaccinatedPerHundred: undefined,
+    };
+  }
+
+  const acceptableDayDifferences = [...Array(input.acceptableDayDifferenceRange).keys()];
+
+  const dayDifferencesWithData = acceptableDayDifferences.flatMap((dayDifference) => {
+    const pastDateToEvaluate = sub(samplingMidDate, {days: dayDifference});
+    const futureDateToEvaluate = add(samplingMidDate, {days: dayDifference});
+
+    const dataForPastDateToEvaluate = input.vaccinationData
+      .find((element) => element.threeLetterCountryCode === input.estimate.threeLetterCountryCode)?.data
+      .find((element) => element.year === pastDateToEvaluate.getUTCFullYear().toString())?.data
+      .find((element) => element.month === (pastDateToEvaluate.getUTCMonth() + 1).toString())?.data
+      .find((element) => element.day === (pastDateToEvaluate.getUTCDate()).toString());
+
+    const dataForFutureDateToEvaluate = input.vaccinationData
+      .find((element) => element.threeLetterCountryCode === input.estimate.threeLetterCountryCode)?.data
+      .find((element) => element.year === futureDateToEvaluate.getUTCFullYear().toString())?.data
+      .find((element) => element.month === (futureDateToEvaluate.getUTCMonth() + 1).toString())?.data
+      .find((element) => element.day === (futureDateToEvaluate.getUTCDate()).toString());
+
+    return [(
+      (
+        dataForPastDateToEvaluate !== undefined &&
+        dataForPastDateToEvaluate.countryPeopleFullyVaccinatedPerHundred !== undefined
+      ) ? {
+        dayDifference: -dayDifference,
+        data: {
+          countryPeopleFullyVaccinatedPerHundred: dataForPastDateToEvaluate.countryPeopleFullyVaccinatedPerHundred,
+        }
+      } : undefined
+    ), (
+      (
+        dataForFutureDateToEvaluate !== undefined &&
+        dataForFutureDateToEvaluate.countryPeopleFullyVaccinatedPerHundred !== undefined
+      ) ? {
+        dayDifference: dayDifference,
+        data: {
+          countryPeopleFullyVaccinatedPerHundred: dataForFutureDateToEvaluate.countryPeopleFullyVaccinatedPerHundred,
+        }
+      } : undefined
+    )];
+  }).filter(<T extends unknown>(element: T | undefined): element is T => element !== undefined)
+
+  const dataForDay = dayDifferencesWithData.find((element) => element.dayDifference === 0)
+
+  if(!!dataForDay) {
+    return {
+      countryPeopleFullyVaccinatedPerHundred: dataForDay.data.countryPeopleFullyVaccinatedPerHundred,
+    }
+  }
+
+  const dayInThePastWithTheLeastDifference = dayDifferencesWithData
+    .filter((element) => element.dayDifference < 0)
+    .sort((a, b) => Math.abs(a.dayDifference) - Math.abs(b.dayDifference))
+    .at(0);
+
+  const dayInTheFutureWithTheLeastDifference = dayDifferencesWithData
+    .filter((element) => element.dayDifference > 0)
+    .sort((a, b) => Math.abs(a.dayDifference) - Math.abs(b.dayDifference))
+    .at(0);
+  
+  if(dayInThePastWithTheLeastDifference && dayInTheFutureWithTheLeastDifference) {
+    // Turn this into a line with a slope of y=mx+b where x is the date.
+    // We approximate the line to be linear on such a small time scale (~14 days).
+
+    // m = dy/dx
+    const m = (dayInTheFutureWithTheLeastDifference.data.countryPeopleFullyVaccinatedPerHundred - dayInThePastWithTheLeastDifference.data.countryPeopleFullyVaccinatedPerHundred) / (dayInTheFutureWithTheLeastDifference.dayDifference - dayInThePastWithTheLeastDifference.dayDifference);
+
+    // b = y-mx
+    const b = (dayInTheFutureWithTheLeastDifference.data.countryPeopleFullyVaccinatedPerHundred) - (m * dayInTheFutureWithTheLeastDifference.dayDifference)
+
+    // y = mx+b, x=0 in this case because the day difference is zero
+    const countryPeopleFullyVaccinatedPerHundred = (m * 0) + b;
+
+    return {
+      countryPeopleFullyVaccinatedPerHundred
+    }
+  }
+
+  // Getting here means we simply didn't have enough data to tell how many positive cases had occurred at this time.
+  return {
+    countryPeopleFullyVaccinatedPerHundred: undefined
+  }
+}
 
 export type EstimateFieldsAfterAddingVaccinationDataStep =
   EstimateFieldsAfterJitteringPinLatLngStep & {
@@ -36,29 +248,32 @@ interface AddVaccinationDataToEstimateStepOutput {
 export const addVaccinationDataToEstimateStep = (
   input: AddVaccinationDataToEstimateStepInput
 ): AddVaccinationDataToEstimateStepOutput => {
+  const acceptableDayDifferenceRange = 7;
+
   console.log(
-    `Running step: addVaccinationDataToEstimateStep. Remaining estimates: ${input.allEstimates.length}`
+    `Running step: addVaccinationDataToEstimateStep. Remaining estimates: ${input.allEstimates.length}. Acceptable day difference range: ${acceptableDayDifferenceRange}`
   );
 
   return {
-    allEstimates: input.allEstimates.map((estimate) => {
-      const {
-        countryPeopleVaccinatedPerHundred,
-        countryPeopleFullyVaccinatedPerHundred
-      } = input.vaccinationData
-        .find((element) => element.threeLetterCountryCode === estimate.countryAlphaThreeCode)?.data
-        .find((element) => estimate.samplingMidDate && element.year === estimate.samplingMidDate.getUTCFullYear().toString())?.data
-        .find((element) => estimate.samplingMidDate && element.month === (estimate.samplingMidDate.getUTCMonth() + 1).toString())?.data
-        .find((element) => estimate.samplingMidDate && element.day === (estimate.samplingMidDate.getUTCDate()).toString()) ?? {
-          countryPeopleVaccinatedPerHundred: undefined,
-          countryPeopleFullyVaccinatedPerHundred: undefined
-        }
-      return {
-        ...estimate,
-        countryPeopleVaccinatedPerHundred,
-        countryPeopleFullyVaccinatedPerHundred
-      }
-    }),
+    allEstimates: input.allEstimates.map((estimate) => ({
+      ...estimate,
+      countryPeopleVaccinatedPerHundred: getAssociatedVaccinationDataForEstimate({
+        estimate: {
+          threeLetterCountryCode: estimate.countryAlphaThreeCode,
+          samplingMidDate: estimate.samplingMidDate
+        },
+        vaccinationData: input.vaccinationData,
+        acceptableDayDifferenceRange
+      }).countryPeopleVaccinatedPerHundred,
+      countryPeopleFullyVaccinatedPerHundred: getAssociatedFullVaccinationDataForEstimate({
+        estimate: {
+          threeLetterCountryCode: estimate.countryAlphaThreeCode,
+          samplingMidDate: estimate.samplingMidDate
+        },
+        vaccinationData: input.vaccinationData,
+        acceptableDayDifferenceRange
+      }).countryPeopleFullyVaccinatedPerHundred
+    })),
     allStudies: input.allStudies,
     vaccinationData: input.vaccinationData,
     positiveCaseData: input.positiveCaseData,
