77bc48f81efb13016e0ba3adab57a205f18e538f
Sean Kenny <86806388+SeanKennyNF@users.noreply.github.com>
2025-03-29 17:36:20 -0600

    Added Kenya location overrides for the Arbo ETL (#134)
    Signed-off-by: SeanKennyNF <sean.kenny@neofinancial.com>

--- src/lib/geocoding-api/geocoding-api-response-overrides.ts
+++ src/lib/geocoding-api/geocoding-api-response-overrides.ts
@@ -18,7 +18,87 @@ const geocodingApiResponseOverrides: Array<{
     text: 'Western Province',
     matchingText: 'Western Province',
     regionName: undefined
-  }
+  },
+}, {
+  parameters: {
+    mapboxSearchText : 'Western Province',
+    countryAlphaTwoCode : 'KE' as const,
+    geocoderDataType : GeocoderDataType.REGION
+  },
+  response: {
+    centerCoordinates: [
+      0.5701902531214326,
+      34.55679540107335
+    ],
+    boundingBox: undefined,
+    text: 'Western Province',
+    matchingText: 'Western Province',
+    regionName: undefined
+  },
+}, {
+  parameters: {
+    mapboxSearchText : 'Coast Province',
+    countryAlphaTwoCode : 'KE' as const,
+    geocoderDataType : GeocoderDataType.REGION
+  },
+  response: {
+    centerCoordinates: [
+      -2.854661737979964,
+      39.614598719130335
+    ],
+    boundingBox: undefined,
+    text: 'Coast Province',
+    matchingText: 'Coast Province',
+    regionName: undefined
+  },
+}, {
+  parameters: {
+    mapboxSearchText : 'North Eastern Province',
+    countryAlphaTwoCode : 'KE' as const,
+    geocoderDataType : GeocoderDataType.REGION
+  },
+  response: {
+    centerCoordinates: [
+      1.1005450767981824,
+      40.25974818773868
+    ],
+    boundingBox: undefined,
+    text: 'North Eastern Province',
+    matchingText: 'North Eastern Province',
+    regionName: undefined
+  },
+}, {
+  parameters: {
+    mapboxSearchText : 'Central Province',
+    countryAlphaTwoCode : 'KE' as const,
+    geocoderDataType : GeocoderDataType.REGION
+  },
+  response: {
+    centerCoordinates: [
+      -0.7862729322527452,
+      36.987418171096714
+    ],
+    boundingBox: undefined,
+    text: 'Central Province',
+    matchingText: 'Central Province',
+    regionName: undefined
+  },
+}, {
+  parameters: {
+    mapboxSearchText : 'Eastern Province',
+    countryAlphaTwoCode : 'KE' as const,
+    geocoderDataType : GeocoderDataType.REGION
+  },
+  response: {
+    centerCoordinates: [
+      1.3903319833457937,
+      38.18368696228271
+    ],
+    boundingBox: undefined,
+    text: 'Eastern Province',
+    matchingText: 'Eastern Province',
+    regionName: undefined
+  },
 }]
 
 interface LookupInGeocodingApiResponseOverrideValuesInput {
